d89394208e799a9ba12832eab0a701b1225061ba
Jovin Cronin-Wilesmith <jcroninwilesmith@goodrx.com>
2021-10-07 16:25:33 -0700

    new query

--- claims.sql
+++ claims.sql
@@ -9,6 +9,17 @@ AND product = 'core'
 AND is_paid_claim = 1
 GROUP BY 1
 
+-- find # of claims by parent pharmacy that we could attribute.
+SELECT 
+parent_pharmacy_name,
+count(*) 
+FROM claim.v_claim 
+WHERE product = 'core' 
+AND common_id IS NOT NULL 
+AND attribution_permission = 'phi_allowed'
+group by 1
+order by 2 desc;
+
 
 -- find claims where coupon viewer's account PII differs from the claimer's
 SELECT * 
